4d225a3ecb97d1c12bf0e86e3795ed2d4148b1e4
liuqingkai <lqk0703@163.com>
2015-06-26 16:32:26 +0800

    集成Swagger

--- seed-project/src/main/java/pw/entrance/seed/controller/HelloWorldController.java
+++ seed-project/src/main/java/pw/entrance/seed/controller/HelloWorldController.java
@@ -2,9 +2,13 @@ package pw.entrance.seed.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import pw.entrance.seed.service.HelloService;
+
+import com.wordnik.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/hello")
@@ -13,7 +17,9 @@ public class HelloWorldController {
 	@Autowired
 	private HelloService helloService;
 	
-	@RequestMapping("/sayhell")
+	@ApiOperation(value = "测试1", httpMethod = "GET")
+	@RequestMapping(value = "/sayhello", method = RequestMethod.GET)
+	@ResponseBody
 	public String sayHello() {
 		return this.helloService.sayHello();
 	}
